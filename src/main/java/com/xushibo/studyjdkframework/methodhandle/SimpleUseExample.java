4183b6e8e6ad9761ab23be28e7cca530a9120fce
ken1993 <1797307763@qq.com>
2022-01-02 09:37:05 +0800

    更新方法句柄案例代码和补充对应文档

--- src/main/java/com/xushibo/studyjdkframework/methodhandle/SimpleUseExample.java
+++ src/main/java/com/xushibo/studyjdkframework/methodhandle/SimpleUseExample.java
@@ -16,12 +16,17 @@ import java.lang.invoke.MethodType;
 public class SimpleUseExample {
 
     public static void main (String[] args) throws NoSuchMethodException, IllegalAccessException {
+        // 初始化测试对象
         Object rcvr = "a";
+        // 方法类型 int
         MethodType mt= MethodType.methodType(int.class);
+        // 寻找上下文
         MethodHandles.Lookup l = MethodHandles.lookup();
+        // 创建测试对象的方法句柄
         MethodHandle mh= l.findVirtual(rcvr.getClass(),"hashCode",mt);
         int ret;
         try {
+            //反射调用
             ret= (int) mh.invoke(rcvr);
             System.out.println(ret);
         } catch (Throwable throwable) {
